50501812cbb7fffa72d493f71f0376c2c9a33601
misganzella <misganzella@gmail.com>
2025-12-03 14:47:46 +0000

    incluse people.csv

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,3 +1,41 @@
+import argparse
+
+
+def load(filepath_):
+    """Load data from filepath to the database"""
+    try:
+        with open(filepath_) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f'File not found {e}')
+
 
 def main():
-    print("Executing dundie from entry points")
+
+    parser = argparse.ArgumentParser(
+    description="Dunder Mifflin Rewards",
+    epilog="Enjoy and use with cautious.",
+)
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help='The subcommand to execute.',
+        choices=['load', 'show', 'send'],
+        default="help"
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+    )
+
+    args = parser.parse_args()
+    globals()[args.subcommand](args.filepath)
+
+    
+
+
+
+if __name__ == "__main":
+    main()
